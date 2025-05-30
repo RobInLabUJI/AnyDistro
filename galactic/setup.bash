5336c778633caff9f720df53ca6019d7ca5bca36
Enric Cervera <ecervera@uji.es>
2025-05-30 16:29:09 +0200

    Add distros

---
+++ galactic/setup.bash
@@ -0,0 +1,6 @@
+DISTRO=galactic
+TAG=$DISTRO-desktop
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/../setup.bash"
+
---
+++ jade/setup.bash
@@ -0,0 +1,6 @@
+DISTRO=jade
+TAG=$DISTRO-desktop-full
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/../setup.bash"
+
