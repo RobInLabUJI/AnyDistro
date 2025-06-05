f3ed872acf80a8d897029b81ae11ea5e4161e3e6
Enric Cervera <ecervera@uji.es>
2025-06-05 15:20:59 +0200

    Keep working directory

--- setup.bash
+++ setup.bash
@@ -1,6 +1,7 @@
 IMAGE_NAME=ros-$DISTRO
 TAG=$DISTRO-desktop-full
 CONTAINER_NAME=$IMAGE_NAME
+WORKDIR=$PWD
 COUNT=$(docker ps | grep "$CONTAINER_NAME" | wc -l)
 
 if [ "$COUNT" -eq 0 ]; then
@@ -10,12 +11,13 @@ if [ "$COUNT" -eq 0 ]; then
     rocker --nvidia --x11 --user --home --nocleanup \
       --image-name $IMAGE_NAME \
       --name $CONTAINER_NAME \
-      osrf/ros:$TAG
+      osrf/ros:$TAG 'bash -c "source /opt/ros/$DISTRO/setup.bash && cd $WORKDIR && bash"'
   else
     docker run -it -v /home/$USER:/home/$USER  --name $CONTAINER_NAME  \
       --gpus all -e DISPLAY -e TERM -e QT_X11_NO_MITSHM=1 \
       -e XAUTHORITY=/tmp/.$IMAGE_NAME.xauth -v /tmp/.$IMAGE_NAME.xauth:/tmp/.$IMAGE_NAME.xauth \
-      -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/localtime:/etc/localtime:ro $IMAGE_NAME 
+      -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/localtime:/etc/localtime:ro $IMAGE_NAME \
+      bash -c "source /opt/ros/$DISTRO/setup.bash && cd $WORKDIR && bash"
   fi
   
   docker commit $CONTAINER_NAME $IMAGE_NAME
@@ -23,7 +25,7 @@ if [ "$COUNT" -eq 0 ]; then
 
 else
 
-  docker exec -it "$CONTAINER_NAME" bash -c "source /opt/ros/$DISTRO/setup.bash && bash"
+  docker exec -it "$CONTAINER_NAME" bash -c "source /opt/ros/$DISTRO/setup.bash && cd $WORKDIR && bash"
 
 fi
 
