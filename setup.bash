c337382667e3b3f260d1ee55d242aa5d7924d132
Enric Cervera <ecervera@uji.es>
2025-06-05 16:42:59 +0200

    Check NVIDIA container toolkit

--- setup.bash
+++ setup.bash
@@ -8,9 +8,14 @@ COUNT=$(docker ps | grep "$CONTAINER_NAME" | wc -l)
 
 if [ "$COUNT" -eq 0 ]; then
 
+  if nvidia-container-toolkit --version &>/dev/null; then
+    ROCKER_NVIDIA="--nvidia"
+    DOCKER_NVIDIA="--gpus all"
+  fi
+
   image_exists=$(docker images -q "$IMAGE_NAME")
   if [ -z "$image_exists" ]; then
-    rocker --nvidia --x11 --user --home --nocleanup \
+    rocker $ROCKER_NVIDIA --x11 --user --home --nocleanup \
       --image-name $IMAGE_NAME \
       --name $CONTAINER_NAME \
       osrf/ros:$TAG "sleep 0"
@@ -21,7 +26,7 @@ if [ "$COUNT" -eq 0 ]; then
 
   docker run -it -v /home/$USER:/home/$USER  --name $CONTAINER_NAME  \
     -h $CONTAINER_NAME \
-    --gpus all -e DISPLAY -e TERM -e QT_X11_NO_MITSHM=1 \
+    $DOCKER_NVIDIA -e DISPLAY -e TERM -e QT_X11_NO_MITSHM=1 \
     -e XAUTHORITY=/tmp/.$IMAGE_NAME.xauth -v /tmp/.$IMAGE_NAME.xauth:/tmp/.$IMAGE_NAME.xauth \
     -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/localtime:/etc/localtime:ro $IMAGE_NAME \
     bash -c "$COMMAND"
