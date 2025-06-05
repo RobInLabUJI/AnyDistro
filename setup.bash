00dc3eac8c7e40dd6b6c74836bc0039b534249e2
Enric Cervera <ecervera@uji.es>
2025-06-05 16:32:59 +0200

    Fix rocker command

--- setup.bash
+++ setup.bash
@@ -2,6 +2,8 @@ IMAGE_NAME=ros-$DISTRO
 TAG=$DISTRO-desktop-full
 CONTAINER_NAME=$IMAGE_NAME
 WORKDIR=$PWD
+COMMAND="source /opt/ros/$DISTRO/setup.bash && cd $WORKDIR && bash"
+ROCKER_COMMAND="bash -c ${COMMAND}"
 COUNT=$(docker ps | grep "$CONTAINER_NAME" | wc -l)
 
 if [ "$COUNT" -eq 0 ]; then
@@ -11,21 +13,25 @@ if [ "$COUNT" -eq 0 ]; then
     rocker --nvidia --x11 --user --home --nocleanup \
       --image-name $IMAGE_NAME \
       --name $CONTAINER_NAME \
-      osrf/ros:$TAG 'bash -c "source /opt/ros/$DISTRO/setup.bash && cd $WORKDIR && bash"'
-  else
-    docker run -it -v /home/$USER:/home/$USER  --name $CONTAINER_NAME  \
-      --gpus all -e DISPLAY -e TERM -e QT_X11_NO_MITSHM=1 \
-      -e XAUTHORITY=/tmp/.$IMAGE_NAME.xauth -v /tmp/.$IMAGE_NAME.xauth:/tmp/.$IMAGE_NAME.xauth \
-      -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/localtime:/etc/localtime:ro $IMAGE_NAME \
-      bash -c "source /opt/ros/$DISTRO/setup.bash && cd $WORKDIR && bash"
+      osrf/ros:$TAG "sleep 0"
+
+    docker commit $CONTAINER_NAME $IMAGE_NAME
+    docker container remove $CONTAINER_NAME
   fi
+
+  docker run -it -v /home/$USER:/home/$USER  --name $CONTAINER_NAME  \
+    -h $CONTAINER_NAME \
+    --gpus all -e DISPLAY -e TERM -e QT_X11_NO_MITSHM=1 \
+    -e XAUTHORITY=/tmp/.$IMAGE_NAME.xauth -v /tmp/.$IMAGE_NAME.xauth:/tmp/.$IMAGE_NAME.xauth \
+    -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/localtime:/etc/localtime:ro $IMAGE_NAME \
+    bash -c "$COMMAND"
   
   docker commit $CONTAINER_NAME $IMAGE_NAME
   docker container remove $CONTAINER_NAME
 
 else
 
-  docker exec -it "$CONTAINER_NAME" bash -c "source /opt/ros/$DISTRO/setup.bash && cd $WORKDIR && bash"
+  docker exec -it "$CONTAINER_NAME" bash -c "$COMMAND"
 
 fi
 
