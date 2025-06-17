8d8faddec174356e6e7a367908f8126d3c475ec1
Enric Cervera <ecervera@uji.es>
2025-06-17 16:25:13 +0200

    Add ROS 2 distros

---
+++ humble/setup.bash
@@ -0,0 +1,5 @@
+DISTRO=humble
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/../setup.bash"
+
---
+++ iron/setup.bash
@@ -0,0 +1,5 @@
+DISTRO=iron
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/../setup.bash"
+
