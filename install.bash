3fc00bc308c72950606a2925d27c784a4f0de5a8
Enric Cervera <ecervera@uji.es>
2025-07-21 08:38:26 +0200

    Change folder

--- install.bash
+++ install.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo cp -R setup.bash indigo jade kinetic lunar melodic noetic foxy galactic humble iron jazzy kilted rolling /opt/ros/.
+sudo mkdir -p /opt/ros-docker && sudo cp -R setup.bash indigo jade kinetic lunar melodic noetic foxy galactic humble iron jazzy kilted rolling /opt/ros-docker/.
 
